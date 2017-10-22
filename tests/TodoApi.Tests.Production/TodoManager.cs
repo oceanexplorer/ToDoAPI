995b63c6b3747019c1b4958846fb400d4ff35db7
Paul Custance <pcustance@gmail.com>
2017-10-22 14:14:05 +0100

    Added production tests and expanded the docker compose test file with sections to the run the tests. The test is currently just a simple call to the healthcheck.

---
+++ tests/TodoApi.Tests.Production/TodoManager.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Http;
+using Xunit;
+using FluentAssertions;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace TodoApi.Tests.Production
+{
+    public class TodoManager
+    {
+        [Fact]
+        public async Task CallingHealthCheckReturnStatus200()
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+
+            var duration = Environment.GetEnvironmentVariable("DURATION");
+            var hostIp = Environment.GetEnvironmentVariable("HOST_IP");
+
+            if(duration != null && duration.Length > 0)
+            {
+                float max;
+                float.TryParse(duration, out max);
+
+                double minutes = 0;
+                int failures = 0;
+                string errorMessage = "";
+
+                while(sw.Elapsed.Minutes < max)
+                {
+                    var httpClient = new HttpClient();                    
+                    var address = $"http://{hostIp}/";
+
+                    httpClient.BaseAddress = new Uri(address);
+                    var response = await httpClient.GetAsync("api/healthcheck");
+
+                    if(!response.IsSuccessStatusCode)
+                    {
+                        failures++;
+                        errorMessage = response.ReasonPhrase;
+                    }
+                    else if (response == null)
+                    {
+                        failures++;
+                        errorMessage = "Got no response";
+                    }  
+                    else
+                    {
+                        response.StatusCode.Should().Be(HttpStatusCode.OK);
+                    }   
+
+                    if(sw.Elapsed.Minutes > minutes)
+                    {
+                        Debug.WriteLine($"{minutes} out of {max} passed");
+                        minutes++;
+                    }
+
+                    if(failures > 1)
+                    {
+                        failures.Should().BeLessThan(1, errorMessage);
+                    }               
+                }
+            }
+            else
+            {
+                var httpClient = new HttpClient();                    
+                var address = $"http://{hostIp}/"; 
+
+                httpClient.BaseAddress = new Uri(address);
+                var response = await httpClient.GetAsync("api/healthcheck");
+
+                if(!response.IsSuccessStatusCode)
+                {                    
+                    response.IsSuccessStatusCode.Should().BeTrue();
+                }
+                else if (response == null)
+                {
+                    response.Should().NotBeNull();
+                }  
+                else
+                {
+                    response.StatusCode.Should().Be(HttpStatusCode.OK);
+                }            
+            }
+        }
+    }
+}
